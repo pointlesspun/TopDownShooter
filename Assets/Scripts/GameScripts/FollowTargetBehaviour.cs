c46a713c0f2e13af41b22ce46ca1211fd3d5e033
Mark Lassche <mark.lassche@damco.com>
2018-11-20 09:29:06 +0100

    camera now follows player (forgot files)

---
+++ Assets/Scripts/GameScripts/FollowTargetBehaviour.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * TDS (c) 2018 by Another Pointless Pun
+ * TDS is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ * You should have received a copy of the license along with this work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+ */
+namespace Tds.GameScripts
+{
+    using UnityEngine;
+
+    public class FollowTargetBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// Object to follow
+        /// </summary>
+        private GameObject _target;
+
+        public void Start()
+        {
+            _target = GameObject.FindGameObjectWithTag(GameTags.Player);
+        }
+
+        public void Update()
+        {
+            // match the target's position as long as the target is alive and active
+            if (_target != null && _target.activeInHierarchy)
+            {
+                var position = _target.transform.position;
+                position.z = transform.position.z;
+                transform.position = position;
+            }
+        }
+    }
+}
